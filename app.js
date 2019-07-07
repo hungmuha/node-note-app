07cd2c921c4fd97838c0af23820c9e76be1b8667
hungmuha <hung.soboss@gmail.com>
2019-07-07 14:09:59 -0600

    added some more changes to create builder and set property of the command and what is being passed to it

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const yargs = require('yargs');
 const getNotes = require("./notes.js");
 
 // const command = process.argv[2];
-console.log(process.argv);
-console.log(yargs.argv);
+// console.log(process.argv);
 
 //Customize yargs version
 yargs.version('1.1.0')
@@ -13,6 +12,48 @@ yargs.version('1.1.0')
 //create add command
 yargs.command({
     command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe:'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log("Title: " + argv.title)
+    }
+})
+
+//create remove command
+yargs.command({
+    command: 'remove',
+    describe:'Remove a note',
+    handler: function() {
+        console.log('Removing the note!')
+    }
+})
+
+//create list command
+yargs.command({
+    command: 'list',
+    describe:'List your notes',
+    handler: function() {
+        console.log('Listing all the notes!')
+    }
+})
+
+//create read command
+yargs.command({
+    command: 'read',
+    describe:'Read a note',
+    handler: function() {
+        console.log('Reading the note!')
+    }
 })
 
 //add, remove, read, list
+
+//parsing content of the arguments
+yargs.parse();
+// console.log(yargs.argv);
